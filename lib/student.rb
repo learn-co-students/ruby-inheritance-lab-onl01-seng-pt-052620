2f0b4437427f62cbd875cec047c36ea67902857f
Ronald Burrell Jr <ronburrell05@gmail.com>
2020-06-24 16:32:17 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
-
+class Student < User
+  attr_accessor :first_name, :last_name
+  
+  def initialize
+    @knowledge = []
+  end
+  
+  def learn(lesson) 
+    @knowledge << lesson
+  end
+  
+  def knowledge
+    @knowledge
+  end
 end
